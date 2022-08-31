783e50e679aa9793088fc96af4eb3c3b8bb396d1
koechronix <koechronix@yahoo.com>
2022-08-31 16:04:45 -0400

    Completed

--- chatterbox-server/app/controllers/application_controller.rb
+++ chatterbox-server/app/controllers/application_controller.rb
@@ -1,6 +1,36 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # Add routes
+  # GET request
+  get '/messages' do
+    message = Message.all.order(:created_at)
+    message.to_json
+  end
+
+  # POST request
+  post '/messages' do
+    message = Message.create(
+      body: params[:body],
+      username: params[:username]
+    )
+    message.to_json
+  end
+
+  # PATCH request
+  patch '/messages/:id' do
+    message = Message.find(params[:id])
+    message.update(
+      body: params[:body]
+    )
+    message.to_json
+  end
+
+  # DELETE request
+  delete '/messages/:id' do
+    message = Message.find(params[:id])
+    message.destroy
+
+    message.to_json
+  end
   
 end
